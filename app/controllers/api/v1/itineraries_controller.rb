4ab54f8cd236efe4a53e64186b1229d5555c21ff
Bennis Yiu <bennisyiu@gmail.com>
2020-08-13 14:48:00 +0800

    login controller moved to api/v1, user_info_path created

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItinerariesController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
   def index
+    @user = User.find(params[:user_id])
     @itineraries = Itinerary.all
     # DESC order (show latest itineraries first)
     # implement search function later, write conditions here - if params[:query] == ?? return ....
@@ -11,22 +12,17 @@ class Api::V1::ItinerariesController < Api::V1::BaseController
   end
 
   def create
-    it_params =  { user_id: itinerary_params[:user_id], date: itinerary_params[:date], name: itinerary_params[:name] }
-    @itinerary = Itinerary.create(it_params)
-    @activity1 = Activity.new(evint_id: itinerary_params[:evint_id1])
-    @activity1.itinerary = @itinerary
-    @activity1.save
-    @activity2 = Activity.new(evint_id: itinerary_params[:evint_id2])
-    @activity2.itinerary = @itinerary
-    @activity2.save
-    @activity3 = Activity.new(evint_id: itinerary_params[:evint_id3])
-    @activity3.itinerary = @itinerary
-    @activity3.save
-    # create an Itenerary
-    # create activities from event IDs (Activity.create(it)
-    # assign activities to the itinerary
-    # save
-
+    # it_params =  { user_id: itinerary_params[:user_id], date: itinerary_params[:date], name: itinerary_params[:name] }
+    @itinerary = Itinerary.create(itinerary_params)
+    # @activity1 = Activity.new(evint_id: itinerary_params[:evint_id1])
+    # @activity1.itinerary = @itinerary
+    # @activity1.save
+    # @activity2 = Activity.new(evint_id: itinerary_params[:evint_id2])
+    # @activity2.itinerary = @itinerary
+    # @activity2.save
+    # @activity3 = Activity.new(evint_id: itinerary_params[:evint_id3])
+    # @activity3.itinerary = @itinerary
+    # @activity3.save
   end
 
   def update
@@ -46,7 +42,6 @@ class Api::V1::ItinerariesController < Api::V1::BaseController
   private
 
   def itinerary_params
-    params.require(:itinerary).permit(:user_id, :date, :name, :evint_id1, :evint_id2, :evint_id3)
+    params.require(:itinerary).permit(:user_id, :date, :name) #, :evint_id1, :evint_id2, :evint_id3)
   end
-
 end
