20908723890622e25c6f14c5ad662695a2c20b60
Bennis Yiu <bennisyiu@gmail.com>
2020-08-13 17:06:22 +0800

    create itinerary and evint index updated

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::ActivitiesController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
-  def create
-    @itinerary = Itinerary.find(params[:itinerary_id])
-    if @itinerary.activities.count < 3
-      @evint = Evint.find(params[:evint_id])
-      @activity = Activity.new(activity_params)
-      @activity.save
-    else
-      render json: { status: "failed", message: "You have already created 3 activities!" }, status: :unprocessable_entity
-    end
-    # first get itinerary by id
-    # check, how mamy activities are in this itinerary
-    # if it is less than 3, allow to create
-  end
+  # def create
+  #   @itinerary = Itinerary.find(params[:itinerary_id])
+  #   if @itinerary.activities.count < 3
+  #     @evint = Evint.find(params[:evint_id])
+  #     @activity = Activity.new(activity_params)
+  #     @activity.save
+  #   else
+  #     render json: { status: "failed", message: "You have already created 3 activities!" }, status: :unprocessable_entity
+  #   end
+  #   ## first get itinerary by id
+  #  ## check, how mamy activities are in this itinerary
+  #   ## if it is less than 3, allow to create
+  # end
 
   def destroy
     @activity = Activity.find(params[:id])
@@ -22,7 +22,7 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
 
   private
 
-  def activity_params
-    params.require(:activity).permit(:itinerary_id, :evint_id)
-  end
+  # def activity_params
+  #   params.require(:activity).permit(:itinerary_id, :evint_id)
+  # end
 end
--- app/controllers/api/v1/evints_controller.rb
+++ app/controllers/api/v1/evints_controller.rb
@@ -1,26 +1,35 @@
 class Api::V1::EvintsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
 
-  def index ## used for both HOME and Add New Itinerary page
-    if params[:query].present?
-        @evints = search
-    elsif params[:date].present? ## filter by date
-      @evints = Evint.where(date: params[:date])
-    else
-      @evints = Evint.all
+  # def index ## used for both HOME and Add New Itinerary page
+  #   if params[:query].present?
+  #       @evints = search
+  #   elsif params[:date].present? ## filter by date
+  #     @evints = Evint.where(date: params[:date])
+  #   else
+  #     @evints = Evint.where("date >= ?", Date.today)
+  #   end
+  #   # since we have past events in the database, only display future events (from Date.today)
+  #   # implement search function later, write conditions here - if params[:query] == ?? return ....
+  # end
+
+  def index
+    @evints = Evint.where("date >= ?", Date.today)
+    if params[:date].present?
+      @evints = @evints.where(date: params[:date])
+    elsif params[:query].present?
+      @evints = search(@evints)
     end
-    # since we have past events in the database, only display future events (from Date.today)
-    # implement search function later, write conditions here - if params[:query] == ?? return ....
   end
 
-  def search
+  def search(evints)
     sql_query = " \
       evints.title @@ :query \
       OR evints.venue @@ :query \
       OR evints.category @@ :query \
       OR evints.description @@ :query \
     "
-  Evint.where(sql_query, query: "%#{params[:query]}%")
+  evints.where(sql_query, query: "%#{params[:query]}%")
   end
 
   def show
