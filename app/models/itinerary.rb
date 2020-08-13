625bd46d32484e11e7050d5a9e8a485a5c1fba17
Bennis Yiu <bennisyiu@gmail.com>
2020-08-13 12:28:14 +0800

    itineraries and activities controller ready. update and destroy to be completed

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,7 @@
 class Itinerary < ApplicationRecord
   belongs_to :user
   has_many :activities
+
+  validates :name, :date, presence: true
+  ## cannot create an itinerary with expired events
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+post '/login', to: 'login#login'
+
    namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :evints, only: [:index, :show]
       resources :itineraries, only: [:index, :show, :create, :update, :destroy]
       resources :activities, only: [:destroy]
-      ## customed routes for activities
+      ## resources :guests
     end
   end
 end
